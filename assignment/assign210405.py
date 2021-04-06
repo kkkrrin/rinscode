7fc232fb192f351c67f2ac0c73ef89d21ae47552
kkkrrin <kijun1017@naver.com>
2021-04-06 21:59:04 +0900

    Update assign210405.py

--- assignment/assign210405.py
+++ assignment/assign210405.py
@@ -1,4 +1,54 @@
-i=0
-while i<3:
-    i=i+1
-    print(i)
+a=1
+b=3
+
+print(a+b)
+
+a = "python is the best choice"
+print(a.find('k'))
+
+b= "hello wolrd"
+print(b.count('l'))
+# 2장 연습문제
+# 1번
+
+len=80
+eng=75
+math=55
+print((len+eng+math)/3)
+
+
+#2번
+a=13
+if a%2==1 :
+    print("나머지가 1이 나오기 때문에 홀수이다.")
+
+else :
+    print("나머지가 0이기 때문에 짝수임.")
+
+print("if문과 나머지 연산을 사용해서 홀,짝을 판별한다.")
+
+#3번 
+
+hongnumber= "881120-1068234"
+birth= hongnumber[0:6]
+print(birth)
+num= hongnumber[7:14]
+print(num)
+
+#4번
+
+hongnumber[7]
+sex=hongnumber[7]
+print(sex)
+
+#5번
+
+a="a:b:c:b"
+result= a.replace(":","#")
+print(result)
+
+
+# print문을 다 써야 하는 건가?, 방법을 몰라서 result 변수에 넣어서 print함.. 더 효율적인 방법이 없나염
+
+
+
